0c0aacbf79fdfa59040305cf56a9a94c413525dc
pvpgod59 <74043995+pvpgod59@users.noreply.github.com>
2023-01-12 19:03:59 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+//  BURGER MENU
+
+const menuHamburger = document.getElementById("menu-hamburger")
+const navLinks = document.getElementById("nav-links")
+const body = document.body
+        
+menuHamburger.addEventListener('click',()=>{navLinks.classList.toggle('mobile-menu')})
+menuHamburger.addEventListener('click',()=>{body.classList.toggle('scroll-block')})
+
+function activate(activer) {
+    document.getElementById("scroll1").classList.remove("active")
+    document.getElementById("scroll2").classList.remove("active")
+    document.getElementById("scroll3").classList.remove("active")
+    document.getElementById("scroll4").classList.remove("active")
+    document.getElementById("scroll5").classList.remove("active")
+    document.getElementById(activer).classList.add("active")
+
+    navLinks.classList.remove("mobile-menu")
+    body.classList.remove("scroll-block")
+}
